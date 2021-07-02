5966c70839c62ae6a0c58ee5528f7c1b9394ba99
JingyiNiu <niu.jy1203@gmail.com>
2021-07-03 11:55:06 +1200

    load data from data.js

---
+++ main.js
@@ -0,0 +1,37 @@
+loadData();
+
+function loadData() {
+  products = [];
+  for (let product of rawdata) {
+    products.push(product);
+  }
+  renderData();
+}
+
+function renderData() {
+  let productItem = "";
+  for (let i of products) {
+    if (i.productMedia[0] && i.productMedia[0].url) {
+      let imgUrl =
+        "https://storage.googleapis.com/luxe_media/wwwroot/" +
+        i.productMedia[0].url;
+      let urlParams =
+        "./detail.html?prodId=" + i.prodId + "&prodTitle=" + i.title;
+
+      productItem += `
+            <div class="display-item" >
+            <a href="details.html">
+              <img
+                src="${imgUrl}"
+                alt="image"
+              />
+              <div class="item-description">
+                <h6>${i.title}</h6>
+                <p>$ ${i.price}</p>
+              </div>
+            </a>
+          </div>`;
+    }
+    document.getElementById("fromData").innerHTML = productItem;
+  }
+}
