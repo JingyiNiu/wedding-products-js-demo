deaf5a52b581c8bcd77b245b9dc75f0dc9a704f7
JingyiNiu <niu.jy1203@gmail.com>
2021-07-03 19:06:30 +1200

    add sort by function

--- main.js
+++ main.js
@@ -25,7 +25,8 @@ function filterCategory() {
     }
     renderData(products);
   }
-  resetFilters();
+  resetPriceFilter();
+  resetSortByFilter();
 }
 
 // price filter
@@ -62,18 +63,42 @@ function filterPrice() {
         break;
     }
   }
-  if (productsFiltered.length == 0) {
-    console.log("empty products array");
-    renderData(productsFiltered);
-  } else {
-    renderData(productsFiltered); // render html with new array
-  }
+  renderData(productsFiltered); // render html with new array
+  resetSortByFilter();
 }
 
 // sort by price low to high
 function sortBy() {
   const sortBy = document.querySelector("#sort-by").value;
   console.log(sortBy);
+  const priceValue = document.querySelector("#price");
+  if (priceValue.selectedIndex == 0) {
+    // 如果还没有按价格范围来筛选，适用products array
+    if (sortBy == "lowtohigh") {
+      products.sort(function (a, b) {
+        return a.price - b.price;
+      });
+    }
+    if (sortBy == "hightolow") {
+      products.reverse(function (a, b) {
+        return a.price - b.price;
+      });
+    }
+    renderData(products);
+  } else {
+    // 如果已经筛选过价格范围，适用productsFiltered array
+    if (sortBy == "lowtohigh") {
+      productsFiltered.sort(function (a, b) {
+        return a.price - b.price;
+      });
+    }
+    if (sortBy == "hightolow") {
+      productsFiltered.reverse(function (a, b) {
+        return a.price - b.price;
+      });
+    }
+    renderData(productsFiltered);
+  }
 }
 
 // render items inside products array to index.html
@@ -109,10 +134,14 @@ function renderData(productsArray) {
   }
 }
 
-// reset price filter and sort-by filter
-function resetFilters() {
+// reset price filter
+function resetPriceFilter() {
   const priceValue = document.querySelector("#price");
-  const soryByValue = document.querySelector("#sort-by");
   priceValue.selectedIndex = 0;
+}
+
+// reset sort-by filter
+function resetSortByFilter() {
+  const soryByValue = document.querySelector("#sort-by");
   soryByValue.selectedIndex = 0;
 }
